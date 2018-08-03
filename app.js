1bebf2e7ce66f917fbfa307a76cff8ffb1b0e94f
w00jt3k <w00jt3k.88@gmail.com>
2018-08-03 19:59:23 +0200

    Asynchronous tasks

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 const puzzleEl = document.querySelector('#puzzle')
 const guessesEl = document.querySelector('#guesses')
 const game1 = new Hangman('Zielona mila', 8)
@@ -14,17 +13,26 @@ window.addEventListener('keypress', (e) => {
     guessesEl.textContent = game1.statusMessage
 })
 
-// Making an HTTP request
-const request = new XMLHttpRequest()
+getPuzzle((error, puzzle) => {
+    if (error) {
+        console.log(`Error: ${error}`)
+    } else {
+        console.log(puzzle)
+    }
+    
+})
 
-request.addEventListener('readystatechange', (e) => {
+// Making an HTTP request
+
+
+/* const request = new XMLHttpRequest()
+
+request.addEventListener('readystatechange', function(e) {
     if (e.target.readyState === 4 && e.target.status === 200) {
-        const data = JSON.parse(e.target.responseText)
-        console.log(data)
-    } else  if (e.target.readyState === 4) {
-        console.log('An error has taken place')
+        const myCountry = JSON.parse(e.target.responseText).find(country => country.alpha2Code === 'PL')
+        console.log(myCountry.name)
     }
 })
 
-request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3')
-request.send()
+request.open('GET', 'http://restcountries.eu/rest/v2/all')
+request.send() */
